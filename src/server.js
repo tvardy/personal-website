1e69fea898bb6a4851e4403c6a6b694ff2d5129c
Grzegorz (Greg) Twardowski <tvardy@users.noreply.github.com>
2020-09-05 09:39:57 +0200

    feat(drafts): populate session data for (dis)allowing drafts

--- src/server.js
+++ src/server.js
@@ -4,10 +4,11 @@ import polka from 'polka'
 import compression from 'compression'
 import helmet from 'helmet'
 import morgan from 'morgan'
+import session from 'express-session'
 import sirv from 'sirv'
 import * as sapper from '@sapper/server'
 
-const { PORT, NODE_ENV } = process.env
+const { PORT, NODE_ENV, SESSION_SECRET } = process.env
 const dev = NODE_ENV === 'development'
 const _next = (_, __, next) => {
   next()
@@ -19,16 +20,45 @@ import { site, api } from './_settings'
 
 const { server } = polka()
   .use(
-    morgan(dev ? 'dev' : 'common'),
+    // gzip compression
     compression({ threshold: 0 }),
+
+    // session handling
+    session({
+      secret: SESSION_SECRET,
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        httpOnly: true,
+        secure: !dev,
+      },
+    }),
+
+    // handling drafts
+    (req, _, next) => {
+      if (SESSION_SECRET in req.query) {
+        req.session.drafts = !req.session.drafts
+      }
+      next()
+    },
+
+    // security rules (for non-dev env only!)
     dev
       ? _next
       : helmet({
           contentSecurityPolicy: false,
         }),
+
+    // a request logger
+    morgan(dev ? 'dev' : 'common'),
+
+    // handling static files
     sirv('public', { dev }),
+
+    // the main reason we're using the backend - SSR
     sapper.middleware({
-      session: () => ({
+      session: (req) => ({
+        drafts: req.session.drafts,
         site,
         api,
       }),
