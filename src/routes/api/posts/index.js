e6d8493e8f451c304dbfb9515166390c683bc0dd
Grzegorz (Greg) Twardowski <tvardy@users.noreply.github.com>
2020-09-05 10:26:07 +0200

    feat(drafts): handle drafts in `api/posts`

--- src/routes/api/posts/index.js
+++ src/routes/api/posts/index.js
@@ -1,7 +1,12 @@
 import PostsService from '../../../services/posts'
 import { sendJSON } from '../../../_utils'
 
-export async function get(req, res) {
-  const data = await PostsService.findAll({ ...req.query })
+export async function get({ query, session }, res) {
+  const data = await PostsService.findAll({ ...query })
+
+  if (!session.drafts) {
+    data.posts = data.posts.filter(({ draft }) => !draft)
+  }
+
   sendJSON(res, data)
 }
--- src/routes/api/posts/tag/[tag].js
+++ src/routes/api/posts/tag/[tag].js
@@ -1,11 +1,16 @@
 import PostsService from '../../../../services/posts'
 import { sendJSON } from '../../../../_utils'
 
-export async function get({ params, query }, res) {
+export async function get({ params, query, session }, res) {
   const { tag } = params
   const data = await PostsService.findAll({
     ...query,
     filters: [{ by: 'tag', value: tag }],
   })
+
+  if (!session.drafts) {
+    data.posts = data.posts.filter(({ draft }) => !draft)
+  }
+
   sendJSON(res, data)
 }
