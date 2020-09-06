300c07969c36b18a2d20dac50ca7d545c707c8ab
Grzegorz (Greg) Twardowski <tvardy@users.noreply.github.com>
2020-09-06 13:16:16 +0200

    chore(bundle size): trim down `highlight.js` language list

--- src/components/Markdown.svelte
+++ src/components/Markdown.svelte
@@ -1,7 +1,27 @@
 <script>
   import md from 'marked'
-  // TODO: trim down the Highlight.js package to only needed elements/languages
-  import highlight from 'highlight.js'
+
+  import highlight from 'highlight.js/lib/core'
+
+  import bash from 'highlight.js/lib/languages/bash'
+  import xml from 'highlight.js/lib/languages/xml'
+  import html from 'highlight.js/lib/languages/htmlbars'
+  import css from 'highlight.js/lib/languages/css'
+  import markdown from 'highlight.js/lib/languages/markdown'
+  import javascript from 'highlight.js/lib/languages/javascript'
+  import plaintext from 'highlight.js/lib/languages/plaintext'
+  import scss from 'highlight.js/lib/languages/scss'
+  import shell from 'highlight.js/lib/languages/shell'
+
+  highlight.registerLanguage('bash', bash)
+  highlight.registerLanguage('css', css)
+  highlight.registerLanguage('xml', xml)
+  highlight.registerLanguage('html', html)
+  highlight.registerLanguage('markdown', markdown)
+  highlight.registerLanguage('javascript', javascript)
+  highlight.registerLanguage('plaintext', plaintext)
+  highlight.registerLanguage('scss', scss)
+  highlight.registerLanguage('shell', shell)
 
   md.setOptions({
     highlight: function (code, lang) {
